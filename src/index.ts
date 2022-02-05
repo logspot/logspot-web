d58f74b0ce258a25aa8ceb499253eb74ee457a16
Bartłomiej Pasik <aproszeciebardzo@gmail.com>
2022-02-05 08:45:21 +0100

    fix: import bug

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,27 @@
-import { getCookie, eraseCookie, setCookie } from "./cookies";
+const setCookie = (name: string, value: string, days: number) => {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+};
+
+const getCookie = (name: string): string | null => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
+
+const eraseCookie = (name: string) => {
+  document.cookie = name + "=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+};
 
 const LOGSPOT_COOKIE_ID = "lgspt_uid";
 const API_URL = "https://api.logspot.io";
